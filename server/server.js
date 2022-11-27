1bb820d1b065d3c41ec77caa2368d2ad2ff0df16
JaQwae <105896967+JaQwae@users.noreply.github.com>
2022-11-27 17:29:33 -0600

    download npm packages switch server around

--- server/server.js
+++ server/server.js
@@ -1,13 +1,14 @@
 const express = require('express');
 const { ApolloServer } = require('apollo-server-express');
+
+const db = require('./config/connection');
 const { typeDefs, resolvers } = require('./schemas')
 
 const path = require('path');
-const db = require('./config/connection');
 const routes = require('./routes');
 
-const app = express();
 const PORT = process.env.PORT || 3001;
+const app = express();
 const server = new ApolloServer({
   typeDefs,
   resolvers
@@ -16,20 +17,25 @@ const server = new ApolloServer({
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-const startApolloServer = async (typeDefs, resolvers) => {
-  await server.start();
-  server.applyMiddleware({ app });
-}
-
 // if we're in production, serve client/build as static assets
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
 
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+});
+
 app.use(routes);
 
-db.once('open', () => {
-  app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
-});
+const startApolloServer = async (typeDefs, resolvers) => {
+  await server.start();
+  server.applyMiddleware({ app });
+
+  db.once('open', () => {
+    app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
+    console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+  });
+}
 
 startApolloServer (typeDefs, resolvers);
